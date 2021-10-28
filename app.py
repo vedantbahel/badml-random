f0075554fcae2bb0ac378bda60e3eca55682d792
Vedant Bahel <vedant556@gmail.com>
2021-10-28 10:41:33 +0530

    minor fix

--- app.py
+++ app.py
@@ -18,10 +18,13 @@ def predict():
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
-    print(prediction[0])
+    if (prediction[0]==0):
+        prediction = "Non-Hazardous"
+    else:
+        prediction = "Hazardous"
 
     #output = round(prediction[0], 2)
-    return render_template('home.html', prediction_text="The nature of asteroid is found to be {}".format(prediction[0]))
+    return render_template('home.html', prediction_text="The nature of asteroid is found to be {}".format(prediction))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
@@ -31,7 +34,11 @@ def predict_api():
     data = request.get_json(force=True)
     prediction = model.predict([np.array(list(data.values()))])
 
-    output = prediction[0]
+    if (prediction[0]==0):
+        output = "Non-Hazardous"
+    else:
+        output = "Hazardous"
+        
     return jsonify(output)
 
 
